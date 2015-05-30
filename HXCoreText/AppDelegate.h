0b321ebcb6d6a7798fabd0bc75e10f4a5ca8c58d
“zmjios” <852838218@qq.com>
2015-05-30 19:54:37 +0800

    Initial Commit

---
+++ HXCoreText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HXCoreText
+//
+//  Created by 曾明剑 on 15/5/30.
+//  Copyright (c) 2015年 zmj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HXCoreText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HXCoreText
+//
+//  Created by 曾明剑 on 15/5/30.
+//  Copyright (c) 2015年 zmj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
